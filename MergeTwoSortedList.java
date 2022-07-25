e431cdb8ffcccca114222d535f37513c85613f75
Stxnt <eddyliberty1@gmail.com>
2022-07-25 19:19:36 -0400

    update

---
+++ MergeTwoSortedList.java
@@ -0,0 +1,50 @@
+
+
+public class MergeTwoSortedList {
+    public static void main(String[] args) {
+        
+    }
+
+    public static ListNode mergeSortedList(ListNode list1, ListNode list2){
+        ListNode temp_node = new ListNode(0);
+        ListNode current_node = temp_node;
+
+
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                current_node.next = list1;
+                list1 = list1.next;
+            } else{
+                current_node.next = list2;
+                list2 = list2.next;
+            }
+
+            current_node = current_node.next;
+        }
+
+        if(list1 != null){
+            current_node.next = list1;
+        }
+
+        if(list2 != null){
+            current_node.next = list2;
+        }
+
+        return temp_node.next;
+    }
+
+
+
+public class ListNode{
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) {
+        this.val = val; 
+        this.next = next;
+    }
+}
+}
+
+
